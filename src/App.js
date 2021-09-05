0507e0a10345219aab5321d1eef0bf249919210b
Sasha Havrylov <sashagavrilov1995@gmail.com>
2021-09-05 13:19:04 +0300

    321

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+      </div>
+    );
+  }
+}
